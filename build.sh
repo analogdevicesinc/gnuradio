f4da605b992d6541d3032a29e6ceac74849e7f04
Adrian Suciu <adrian.suciu@analog.com>
2021-01-22 13:55:12 +0200

    ci: try latest boost
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- build.sh
+++ build.sh
@@ -25,11 +25,11 @@ DEPENDENCIES="
 	python3 \
 	python3-mako \
 	python3-six \
+	mingw-w64-${ARCH}-boost \
 	"
 
 $CC --version
 pacman --needed --noconfirm -Sy ${DEPENDENCIES}
-pacman -U --noconfirm http://repo.msys2.org/mingw/$ARCH/mingw-w64-$ARCH-boost-1.72.0-3-any.pkg.tar.zst
 
 build_log4cpp() {
 	git clone https://github.com/orocos-toolchain/log4cpp ${WORKDIR}/log4cpp
