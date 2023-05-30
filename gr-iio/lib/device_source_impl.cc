6ccfbe6746c2d2b947529e2c79216206f13198db
Adrian Suciu <adrian.suciu@analog.com>
2023-05-30 17:56:13 +0300

    iio: make gr::io_signature for device_source dynamic
    This modification enables using devices that have arbitrary channel size
    or channels with different sizes accross the same device
    
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- gr-iio/lib/device_source_impl.cc
+++ gr-iio/lib/device_source_impl.cc
@@ -12,6 +12,7 @@
 #endif
 
 #include "device_source_impl.h"
+#include "iio_priv.h"
 #include <gnuradio/io_signature.h>
 #include <gnuradio/thread/thread.h>
 
@@ -177,6 +178,7 @@ device_source_impl::device_source_impl(iio_context* ctx,
       thread_stopped(false)
 {
     unsigned int nb_channels, i;
+    std::vector<int> signature;
 
     if (!ctx)
         throw std::runtime_error("Unable to create context");
@@ -200,6 +202,8 @@ device_source_impl::device_source_impl(iio_context* ctx,
 
             iio_channel_enable(chn);
             channel_list.push_back(chn);
+
+            signature.push_back(channel_size(chn));
         }
     } else {
         for (std::vector<std::string>::const_iterator it = channels.begin();
@@ -214,11 +218,14 @@ device_source_impl::device_source_impl(iio_context* ctx,
 
             iio_channel_enable(chn);
             channel_list.push_back(chn);
+
+            signature.push_back(channel_size(chn));
         }
     }
 
     set_params(params);
     set_output_multiple(0x400);
+    set_output_signature(gr::io_signature::makev(1, -1, signature));
 
     message_port_register_out(port_id);
 }
